cb9a4486d92624039c03b2a08271bcb5070ffa59
강동윤 <kdy1997.dev@gmail.com>
2020-02-01 09:29:58 +0900

    Detect imports

--- spack/src/import.rs
+++ spack/src/import.rs
@@ -1,7 +1,9 @@
 use super::Bundler;
 use std::mem::replace;
+use swc_atoms::js_word;
 use swc_common::{util::move_map::MoveMap, Fold, FoldWith};
 use swc_ecma_ast::*;
+use swc_ecma_utils::find_ids;
 
 impl Bundler {
     /// This methods removes import statements (statements like `import a as b
@@ -21,7 +23,8 @@ impl Bundler {
 #[derive(Default)]
 pub(super) struct ImportInfo {
     pub imports: Vec<ImportDecl>,
-    pub requires: Vec<ImportDecl>,
+    pub requires: Vec<Str>,
+    pub partial_requires: Vec<ImportDecl>,
     pub dynamic_imports: Vec<Str>,
 }
 
@@ -43,7 +46,42 @@ impl Fold<Vec<ModuleItem>> for ImportFinder {
     }
 }
 
-impl Fold<CallExpr> for ImportFinder {}
+impl Fold<CallExpr> for ImportFinder {
+    fn fold(&mut self, node: CallExpr) -> CallExpr {
+        if node.args.len() != 1 {
+            return node;
+        }
+        let src = match node.args.first().unwrap() {
+            ExprOrSpread {
+                spread: None,
+                expr: box Expr::Lit(Lit::Str(s)),
+            } => s,
+            _ => return node,
+        };
+
+        match node.callee {
+            ExprOrSuper::Expr(box Expr::Ident(Ident {
+                sym: js_word!("require"),
+                ..
+            })) => {
+                self.info.requires.push(src.clone());
+                return node;
+            }
+
+            ExprOrSuper::Expr(box Expr::Ident(Ident {
+                sym: js_word!("import"),
+                ..
+            })) => {
+                self.info.dynamic_imports.push(src.clone());
+                return node;
+            }
+
+            _ => {}
+        }
+
+        node.fold_children(self)
+    }
+}
 
 /// ```js
 /// const { readFile } = required('fs');
@@ -58,13 +96,47 @@ impl Fold<VarDeclarator> for ImportFinder {
     fn fold(&mut self, node: VarDeclarator) -> VarDeclarator {
         match node.init {
             Some(box Expr::Call(CallExpr {
+                span,
                 callee:
                     ExprOrSuper::Expr(box Expr::Ident(Ident {
                         sym: js_word!("require"),
                         ..
                     })),
+                ref args,
                 ..
-            })) => {}
+            })) if args.len() == 1 => {
+                let src = match args.first().unwrap() {
+                    ExprOrSpread {
+                        spread: None,
+                        expr: box Expr::Lit(Lit::Str(s)),
+                    } => s.clone(),
+                    _ => return node,
+                };
+
+                let ids: Vec<Ident> = find_ids(&node.name);
+
+                self.info.partial_requires.push(ImportDecl {
+                    span,
+                    specifiers: ids
+                        .into_iter()
+                        .map(|ident| {
+                            ImportSpecifier::Specific(ImportSpecific {
+                                span,
+                                local: ident,
+                                imported: None,
+                            })
+                        })
+                        .collect(),
+                    src,
+                });
+
+                return VarDeclarator {
+                    name: node.name.fold_with(self),
+                    ..node
+                };
+            }
+
+            _ => {}
         }
 
         node.fold_children(self)
