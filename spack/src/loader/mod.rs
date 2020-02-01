3ee21dddb9eea6b87f8c157a8e9472a25478f8b9
강동윤 <kdy1997.dev@gmail.com>
2020-02-01 10:34:35 +0900

    fixup! Arc<Handler>

--- spack/src/loader/mod.rs
+++ spack/src/loader/mod.rs
@@ -1,37 +1,22 @@
 use anyhow::Error;
-use std::{path::PathBuf, sync::Arc};
-use swc_common::{SourceFile, SourceMap};
+use std::{path::Path, sync::Arc};
+use swc_common::SourceFile;
 use swc_ecma_ast::Module;
 
 /// Implementors of [Load] should not try parallel loading.
 pub trait Load {
-    fn load(
-        &self,
-        cm: &Arc<SourceMap>,
-        base: &PathBuf,
-        import: &str,
-    ) -> Result<(Arc<SourceFile>, Module), Error>;
+    fn load(&self, base: &Path, import: &str) -> Result<(Arc<SourceFile>, Module), Error>;
 }
 
 impl<T: ?Sized + Load> Load for Box<T> {
-    fn load(
-        &self,
-        cm: &Arc<SourceMap>,
-        base: &PathBuf,
-        import: &str,
-    ) -> Result<(Arc<SourceFile>, Module), Error> {
-        T::load(self, cm, base, import)
+    fn load(&self, base: &Path, import: &str) -> Result<(Arc<SourceFile>, Module), Error> {
+        T::load(self, base, import)
     }
 }
 
 impl<'a, T: ?Sized + Load> Load for &'a mut T {
-    fn load(
-        &self,
-        cm: &Arc<SourceMap>,
-        base: &PathBuf,
-        import: &str,
-    ) -> Result<(Arc<SourceFile>, Module), Error> {
-        T::load(self, cm, base, import)
+    fn load(&self, base: &Path, import: &str) -> Result<(Arc<SourceFile>, Module), Error> {
+        T::load(self, base, import)
     }
 }
 
