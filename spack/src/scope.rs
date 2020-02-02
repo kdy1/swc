be34abcaddc40425c5018b68af21f08709512df8
강동윤 <kdy1997.dev@gmail.com>
2020-02-02 09:27:19 +0900

    Add Scope.module_marks

--- spack/src/scope.rs
+++ spack/src/scope.rs
@@ -1,13 +1,19 @@
 use super::Bundler;
 use crate::{transform::TransformedModule, Id, ModuleId, QualifiedId};
 use dashmap::DashMap;
+use fxhash::FxBuildHasher;
 use std::{path::PathBuf, sync::Arc};
+use swc_common::Mark;
 use swc_ecma_ast::Lit;
 
 #[derive(Debug, Default)]
 pub(super) struct Scope {
     pure_constants: DashMap<QualifiedId, Lit>,
+    /// Phase 1 cache
     pub cache: DashMap<Arc<PathBuf>, TransformedModule>,
+
+    /// Marks applied to bindings
+    pub module_marks: DashMap<ModuleId, Mark, FxBuildHasher>,
 }
 
 impl Bundler {
