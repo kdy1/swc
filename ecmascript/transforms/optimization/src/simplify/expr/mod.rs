b9ec1285a80e76d94509b7830f20a734f176c5af
강동윤 <kdy1997.dev@gmail.com>
2021-01-28 17:23:15 +0900

    fixup

--- ecmascript/transforms/optimization/src/simplify/expr/mod.rs
+++ ecmascript/transforms/optimization/src/simplify/expr/mod.rs
@@ -1157,7 +1157,7 @@ impl Fold for SimplifyExpr {
                         _ => ps.push(p),
                     }
                 }
-
+                self.changed = true;
                 ObjectLit { span, props: ps }.into()
             }
 
@@ -1170,6 +1170,7 @@ impl Fold for SimplifyExpr {
                 {
                     let e = &*e.args.into_iter().next().unwrap().pop().unwrap().expr;
                     if let Known(value) = e.as_string() {
+                        self.changed = true;
                         return Expr::Lit(Lit::Str(Str {
                             span: e.span(),
                             value: value.into(),
