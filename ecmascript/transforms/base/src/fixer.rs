116990ad8273ec6809ae888c82c954b9f51fe33c
강동윤 <kdy1997.dev@gmail.com>
2021-07-03 13:24:09 +0900

    fixer: Handle `in`

--- ecmascript/transforms/base/src/fixer.rs
+++ ecmascript/transforms/base/src/fixer.rs
@@ -180,7 +180,10 @@ impl VisitMut for Fixer<'_> {
         };
 
         match &mut *expr.left {
-            Expr::Bin(BinExpr { op: op!("??"), .. }) => {
+            Expr::Bin(BinExpr {
+                op: op!("??") | op!("in"),
+                ..
+            }) => {
                 self.wrap(&mut expr.left);
             }
 
