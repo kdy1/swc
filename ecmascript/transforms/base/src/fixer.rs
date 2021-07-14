898daf6d3dc22b93c418e6b6d04072c46149b277
강동윤 <kdy1997.dev@gmail.com>
2021-07-14 16:10:21 +0900

    Fixer: remove more parens

--- ecmascript/transforms/base/src/fixer.rs
+++ ecmascript/transforms/base/src/fixer.rs
@@ -171,7 +171,7 @@ impl VisitMut for Fixer<'_> {
 
         match expr.op {
             op!("||") | op!("&&") => match (&*expr.left, &*expr.right) {
-                (Expr::Update(..), Expr::Assign(..)) => {
+                (Expr::Update(..), Expr::Assign(..) | Expr::Call(..)) => {
                     self.wrap(&mut expr.right);
                     return;
                 }
@@ -1298,6 +1298,11 @@ var store = global[SHARED] || (global[SHARED] = {});
     test_fixer!(minifier_006, "-('s'/'b')", "-('s'/'b')");
 
     test_fixer!(minifier_007, "(void 0) === value", "void 0 === value");
-
     test_fixer!(minifier_008, "(size--) && (b = (c))", "size-- && (b = c)");
+
+    test_fixer!(
+        minifier_009,
+        "(--remaining) || deferred.resolveWith()",
+        "--remaining || deferred.resolveWith()"
+    );
 }
