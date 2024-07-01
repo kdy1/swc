721e5b7e7247f24c3da1935cb1880a901a4d6fbb
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-01 11:25:09 +0900

    Add a testx

---
+++ crates/swc_ecma_transforms_compat/tests/async-to-generator/issue-9110/input.js
@@ -0,0 +1,30 @@
+function* test() {
+    while (!False()) {
+        // execute this line
+        while (!False()) {
+            // execute this line
+            break;
+        }
+        // execute this line
+        if (False()) {
+            // NOT execute this line
+            break;
+        }
+
+        // execute this line
+        yield "correct";
+        return;
+    }
+
+    // NOT execute this line
+    yield "wrong";
+    return;
+}
+
+function False() {
+    return false;
+}
+
+for (const result of test()) {
+    console.log(result);
+}
