dda6d76ed33fcbb61feb7c22a61350fc4770e91e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-24 06:29:53 +0900

    okay

--- bindings/binding_core_node/src/estree.rs
+++ bindings/binding_core_node/src/estree.rs
@@ -4,12 +4,28 @@ use swc_core::ecma::ast::{Program, Script, Stmt};
 
 #[napi]
 pub struct JsProgram {
-    program: Program,
+    inner: Program,
 }
 
 #[napi]
 pub struct JsScript {
     inner: SharedReference<JsProgram, Script>,
+}
+
+#[napi]
+impl JsScript {
+    #[napi]
+    pub fn stmts(&self, reference: Reference<JsProgram>, env: Env) -> Result<JsStmts> {
+        Ok(JsStmts {
+            inner: reference
+                .share_with(env, |program| Ok(&*program.inner.as_script().unwrap().body))?,
+        })
+    }
+}
+
+#[napi]
+pub struct JsStmts {
+    inner: SharedReference<JsProgram, &'static [Stmt]>,
 }
 
 #[napi]
